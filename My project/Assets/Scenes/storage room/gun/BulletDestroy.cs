a3b14a6c5d389bd470d168cce86f7f3c3fc19bce
Alex Han <alex.han@epita.fr>
2025-05-24 16:57:21 +0200

    Removed "my project" useless

--- My project/Assets/Scenes/storage room/gun/BulletDestroy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class BulletDestroy : MonoBehaviour
-{
-    void OnCollisionEnter(Collision collision)
-    {
-        Destroy(gameObject); // Destroy bullet on any collision
-    }
-}
